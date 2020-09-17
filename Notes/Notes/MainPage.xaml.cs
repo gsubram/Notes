63297de1a77c4d268c9cbdf1a5e9fca20414751b
gsubram <g.n.subramanian@gmail.com>
2020-09-16 22:40:13 -0400

    Add project files.

---
+++ Notes/Notes/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace Notes
+{
+    public partial class MainPage : ContentPage
+    {
+        //_filename refers to where the file will be stored\
+        //Path.Combine concatenates variables together into single string
+        string _fileName = Path.Combine(
+            Environment.GetFolderPath(
+                Environment.SpecialFolder.LocalApplicationData
+                ), "notes.txt"
+            );
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //display file contents if file exists
+            if (File.Exists(_fileName))
+            {
+                editor.Text = File.ReadAllText(_fileName);
+            }
+        }
+
+        void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            File.WriteAllText(_fileName, editor.Text);
+        }
+
+        void OnReloadButtonClicked(object sender, EventArgs e)
+        {
+            if (File.Exists(_fileName))
+            {
+                editor.Text = string.Empty;
+                editor.Text = File.ReadAllText(_fileName);
+            }
+        }
+
+        //delete file if it exists and clear editor textbox
+        void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            if (File.Exists(_fileName))
+            {
+                File.Delete(_fileName);
+            }
+            editor.Text = string.Empty;
+        }
+    }
+}
