f79f5e988d59be581e9f422e675b9cc1630c0b8b
gsubram <g.n.subramanian@gmail.com>
2020-09-17 15:14:20 -0400

    Reconfigured backend so that notes are no longer stored separately in memory but are within a database. Also updated date/time associated with notes to last updated from when created.

--- Notes/Notes/Models/Note.cs
+++ Notes/Notes/Models/Note.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using SQLite;
 
 namespace Notes.Models
 {
     //defines a Note model that will store data about each note in app
     public class Note
     {
-        public string Filename { get; set; }
+        [PrimaryKey,AutoIncrement]
+        public int ID { get; set; }
         public string Text { get; set; }
         public DateTime Date { get; set; }
 
